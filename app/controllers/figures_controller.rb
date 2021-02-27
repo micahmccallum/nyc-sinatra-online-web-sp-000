c6508dc115f9b286b017ed95964b9f8f0c9d578a
Micah <micahmccallum@outlook.com>
2021-02-26 20:31:03 -0500

    commit tree c8bb20ebc68ab55e858cd060a4694a9134b4a017

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,46 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all 
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @figure.titles << Title.create(params[:title]) if !params[:title][:name].empty?
+    @figure.landmarks << Landmark.create(params[:landmark]) if !params[:landmark][:name].empty?
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do    
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    @figure.landmark_ids = params[:figure][:landmark_ids]
+    @figure.landmarks << Landmarks.create(params[:landmark]) if !params[:landmark][:name].empty?
+    @figure.title_ids = params[:figure][:title_ids]    
+    @figure.titles << Title.create(params[:title]) if !params[:title][:name].empty?
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+
 end
